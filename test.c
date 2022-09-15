62ba00cb7727e613cd6044c2e8d00fa829726550
Tejas Srivastava <tejas591995@gmail.com>
2022-09-16 00:10:14 +0530

    First Commit

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+// defining colours
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+typedef long long ll;
+typedef long double ld;
+
+
+int main()
+{
+    printf("\e[?25h");
+
+
+    return 0;
+}

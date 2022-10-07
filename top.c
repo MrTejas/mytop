85377bb6aa07eb4c4f22a731c5cc399c14a5750c
Tejas Srivastava <tejas591995@gmail.com>
2022-10-07 10:59:35 +0530

    More Features added

--- top.c
+++ top.c
@@ -36,11 +36,14 @@ struct process
     char* state;
     int pid; // pid of the process
     int ppid; // pid of the parent of the process
-    double mem; //memory percentage used by the process
     double cpu; //cpu percentage used by the process
     int tty; // the tty terminal process is using
     ll time; // stores the running time of the process in seconds
     int priority; // stores the priority for realtime/non realtime scheduled processes
+    int nice; // nice value of priority range {19 to -20} (High to Low) 
+    int user_id; // user id of the user using the process
+    ll res; // resident memory of the process
+    ll virt; // virtual memory of the process
 };
 
 
@@ -54,7 +57,6 @@ proc createProcess(int pid)
     }
     char nil[20]="nil";
     P->cpu=-1;
-    P->mem=-1;
     P->pid=pid;
     P->ppid=-1;
     P->tty=-1;
@@ -62,20 +64,67 @@ proc createProcess(int pid)
     P->name = (char*)malloc(20*sizeof(char));
     P->state = (char*)malloc(5*sizeof(char));
     P->time=-1;
+    P->user_id=-1;
     P->priority=101;
+    P->nice = 21;
+    P->res=-1;
+    P->virt=-1;
 
     return P;
 }
 
 
-// to work upon
-void updateProcess(int pid, proc P)
+void getProcessMemory(int pid,ll* res, ll* vm)
 {
     char path[20] = "/proc/";
     char str[10];
     sprintf(str,"%d",pid);
     strcat(path,str);
+    strcat(path,"/status");
+    
+    FILE* fp;
+    fp = fopen(path,"r");
+
+    if(fp==NULL)
+    {
+        printf("could not open file %s\n",path);
+        return;
+    }
+
+    char line[256];
+    while(fgets(line,sizeof(line),fp))
+    {
+        char word[30];
+        ll val;
+        sscanf(line,"%s %lld",word,&val);
+
+        if(strcmp(word,"VmSize:")==0)
+        {
+            *vm=val;
+        }
+        else if(strcmp(word,"VmRSS:")==0)
+        {
+            *res=val;
+            break;
+        }
+    }
+    fclose(fp);
+}
+
+// to work upon
+void updateProcess(int pid, proc P)
+{
+    char path[20] = "/proc/";
+    char path1[20] = "/proc/";
+    char str[10];
+    sprintf(str,"%d",pid);
+    
+    strcat(path,str);
+    strcat(path1,str);
+    
     strcat(path,"/stat");
+    strcat(path1,"/loginuid");
+
     FILE* fp;
     fp = fopen(path,"r");
     // printf("path = %s\n",path);
@@ -85,7 +134,13 @@ void updateProcess(int pid, proc P)
     char temp[10];
     sscanf(line,"%s%s%s",temp,P->name,P->state);
 
-    printf("temp = %s\nP->name = %s\nP->state = %s\n",temp,P->name,P->state);
+    FILE* lid = fopen(path1,"r");
+    char string_uid [10];
+    fgets(line,sizeof(line),lid);
+    sscanf(line,"%d",&P->user_id);
+    fclose(lid);
+
+    // printf("temp = %s\nP->name = %s\nP->state = %s\n",temp,P->name,P->state);
     
     // fp = fopen("/proc/4378/stat","r");
 
@@ -107,7 +162,7 @@ void updateProcess(int pid, proc P)
     printf("ck10\n");
     ld process_utime,process_stime,process_starttime;
     ld ticks = sysconf( _SC_CLK_TCK );
-    printf("ticks = %Lf\n",ticks);
+    // printf("ticks = %Lf\n",ticks);
     FILE* ut = fopen("/proc/uptime","r");
     ld system_uptime;
     fscanf(ut,"%Lf",&system_uptime);
@@ -131,6 +186,10 @@ void updateProcess(int pid, proc P)
         {
             P->priority=val;
         }
+        else if(count==16)
+        {
+            P->nice=val;
+        }
         else if(count==6)
         {
             P->tty=val;
@@ -138,18 +197,19 @@ void updateProcess(int pid, proc P)
         else if(count==11)
         {
             process_utime=val/ticks;
-            printf("count = %d\tval = %lld\n",count,val);
+            // printf("count = %d\tval = %lld\n",count,val);
         }
         else if(count==12)
         {
             process_stime=val/ticks;
-            printf("count = %d\tval = %lld\n",count,val);
+            // printf("count = %d\tval = %lld\n",count,val);
         }
         else if(count==19)
         {
             process_starttime=val/ticks;
-            printf("count = %d\tval = %lld\n",count,val);
+            // printf("count = %d\tval = %lld\n",count,val);
         }
+        
 
         // cpu percentage calculation
         
@@ -165,6 +225,10 @@ void updateProcess(int pid, proc P)
         fscanf (fpp, "%lld", &val);
     }
     fclose (fpp);  
+
+    // obtaining memory figures of the pid
+    getProcessMemory(P->pid,&P->res,&P->virt);
+
     
 }
 
@@ -174,8 +238,8 @@ void displayProcess(proc P)
     {
         printf("\n************************\n");
         printf("Displaying Process...\n");
-        printf("pid = %d\nppid = %d\nmem = %f\ncpu = %f\n",P->pid,P->ppid,P->mem,P->cpu);
-        printf("tty = %d\ntime = %lld s\npriority = %d\n",P->tty,P->time,P->priority);
+        printf("pid = \t\t%d\nppid = \t\t%d\ncpu = \t\t%.2f%%\n",P->pid,P->ppid,P->cpu);
+        printf("tty = \t\t%d\ntime = \t\t%lld s\npriority = \t%d\nnice = \t\t%d\nuser_id : \t%d\nres : \t\t%lld kb\nvirt : \t\t%lld kb\n",P->tty,P->time,P->priority,P->nice,P->user_id,P->res,P->virt);
         if(P->command!=NULL)
         {
             printf("command : %s\n",P->command);
@@ -257,6 +321,7 @@ ll* getMemInfo()
     meminfo = fopen("/proc/meminfo","r");
     if(meminfo==NULL)
     {
+        printf("could not open file /proc/meminfo\n");
         return NULL;
     }
     ll* ar = (ll*)malloc(sizeof(ll)*4);
@@ -321,10 +386,10 @@ int* getUptimes()
 
 
 // main method
-int main1 ()
+int main()
 {
-    // hiding the console cursor
-    printf("\33[?25l");
+    // hiding the console cursor (UNCOMMENT IN THE END)
+    // printf("\33[?25l");
 
     // initializong the global constants
     num_cores = getNumCores();
@@ -370,7 +435,12 @@ int main1 ()
             char bat_status[20];
             int bat_percentage;
             getBatteryStats(&bat_percentage,bat_status,bat_level);
-            printf("\nBattery Stats : %s - %d%% (%s)\n",bat_status,bat_percentage,bat_level);
+            printf("Battery Stats : %s - %d%% (%s)\n",bat_status,bat_percentage,bat_level);
+
+
+            // ****************************Process Details******************************
+            printf("\n\n");
+            printf("PID\tUSER\tPRI\tNI\tRES\tSTATE\tCPU%%\tMEM%%\tTIME\tCOMMAND\n");
             
         }
     }
@@ -381,13 +451,14 @@ int main1 ()
 }
 
 
-int main()
+int main1()
 {
+    printf("\33[?25h");
     // proc P = (proc)malloc(sizeof(struct process));
     printf("ck1\n");
-    proc P = createProcess(1821);
+    proc P = createProcess(1876);
     printf("ck2\n");
-    updateProcess(1821,P);
+    updateProcess(1876,P);
     displayProcess(P);
     return 0;
 
